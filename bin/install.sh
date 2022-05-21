74efc57b3da5b2227a687955cc790a75627e4596
Alexandre Dulaunoy <a@foo.be>
2022-05-21 16:57:06 +0200

    chg: [kvrocks] update the kvrocks install script

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/KvrocksLabs/kvrocks.git
+if [[ ! ./kvrocks ]]
+then
+   git clone --recursive https://github.com/apache/incubator-kvrocks.git kvrocks
+fi
 cd kvrocks
-git checkout 2.0.1
-make -j4
+git pull
+git submodule update
+#git checkout 2.0.1
+#make -j4
+mkdir build
+./build.sh ./build
--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,2 @@
 ulimit -n 700000
-./kvrocks/src/kvrocks -c ../etc/kvrocks.conf
+./kvrocks/build/kvrocks -c ../etc/kvrocks.conf
